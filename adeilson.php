fc22fbb52fdf007424d6928772075e057d0c912a
Avelino Gomes <102837037+avelinogomes2005@users.noreply.github.com>
2022-11-09 21:43:52 -0300

    Create adeilson.php

---
+++ adeilson.php
@@ -0,0 +1,3 @@
+<button class="btn btn-warning">Deletar</button>
+<button class="btn btn-primary">Editar</button>
+<a href="https://github.com/adeilsoara"><button class="btn btn-danger">Mandar o Hitman</button></a>
