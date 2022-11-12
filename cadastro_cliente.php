984753bc5aafb44777e4336449684aa407b254ae
Avelino Gomes <102837037+avelinogomes2005@users.noreply.github.com>
2022-11-12 15:31:07 -0300

    Create cadastro_cliente.php

---
+++ cadastro_cliente.php
@@ -0,0 +1,23 @@
+<?php
+
+$nome = $_POST["nome"];
+$capital = $_POST["capital"];
+$cpf = $_POST["cpf"];
+$endereco = $_POST["endereco"];
+$tel = $_POST["tel"];
+$tempo = $_POST["tempo"];
+$email = $_POST["email"];
+$senha = $_POST["senha"];
+
+echo $nome . "\n";
+echo $capital . "\n";
+echo $cpf . "\n";
+echo $endereco . "\n";
+echo $tel . "\n";
+echo $tempo . "\n";
+echo $email . "\n";
+echo $senha . "\n";
+
+
+
+?>
